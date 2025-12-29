7e31af4cca929b3740e52773e4f69b5c5cf80c51
raghav-0905 <sharmaraghav8512@gmail.com>
2025-12-29 20:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.82%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
         int row = nums.length + 2;
-        int col = (nums.length + 3) / 2;
-        int[][] c = new int[row][col];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < col; j++) {
-                c[i][j] = 0;
-            }
+        int[] dp = new int[row];
+        for(int i = 2; i<row; i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-2]);
         }
-        for (int i = 2; i < row; i++){
-            c[i][0]=0;
-        }
-        for(int i =2; i<row; i++){
-            for(int j = 1; j<col; j++){
-                c[i][j]=Math.max(c[i-1][j],(nums[i-2]+c[i-2][j-1]));
-            }
-        }
-        return c[row - 1][col - 1];
+        return dp[row-1];
     }
 }

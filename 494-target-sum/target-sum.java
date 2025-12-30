bc11620cd6fd71f1179e4dbf3f271d0a9630be63
raghav-0905 <sharmaraghav8512@gmail.com>
2025-12-30 16:59:38 +0530

    Time: 5 ms (80.59%) | Memory: 46.6 MB (30.83%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,12 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums,0,target);
-    }
-    public int solve(int[] nums, int index, int target){
-        if(index==nums.length){
-            if(target==0)return 1;
-            return 0;
+        int n = nums.length;
+
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        // If target is outside possible range
+        if (Math.abs(target) > sum) return 0;
+
+        int offset = sum;
+        int[][] dp = new int[n + 1][2 * sum + 1];
+
+        // Base case: using 0 numbers, sum = 0
+        dp[0][offset] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int val = nums[i - 1];
+            for (int j = 0; j <= 2 * sum; j++) {
+                if (dp[i - 1][j] != 0) {
+                    dp[i][j + val] += dp[i - 1][j];
+                    dp[i][j - val] += dp[i - 1][j];
+                }
+            }
         }
-        return solve(nums,index+1,target-nums[index])+solve(nums,index+1,target+nums[index]);
+
+        return dp[n][target + offset];
     }
 }
